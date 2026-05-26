ec87fabcbc96b796415a22bb5d28193c41d87e3b
wjwon1008 <138737627+wjwon1008@users.noreply.github.com>
2026-05-26 10:25:50 +0900

    Update my_stl1.py

--- my_stl1.py
+++ my_stl1.py
@@ -3,3 +3,16 @@ import streamlit as st
 st.title('웹웹')
 
 'GENESIS MAGMA RACING'
+
+'# :blue[시각화 라이브러리]'
+'#### :orange[Matplotlib: sy.pyplot()]'
+
+import matplotlib.pyplot as plt
+
+x= np.linspace(0, 10, 100)
+y= np.sin(x)
+
+fig, ax = plt.subplots()
+
+ax.plot(x, y)
+st.pyplot(fig) # 차트 출력
